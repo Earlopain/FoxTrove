6430544ca799ca01ea89d7ea6ca8c240e1481249
Earlopain <earlopain@protonmail.com>
2021-09-07 19:53:21 +0200

    [Install] Setup jquery

---
+++ app/assets/javascripts/packs/application.js
@@ -0,0 +1,8 @@
+import $ from "jquery";
+window.$ = $;
+window.jQuery = $;
+
+
+import { } from "../test";
+
+console.log("Hello World");
---
+++ app/assets/javascripts/test.js
@@ -0,0 +1,4 @@
+window.addEventListener("DOMContentLoaded", () => {
+    console.log("Hello Test!");
+    console.log($);
+});
