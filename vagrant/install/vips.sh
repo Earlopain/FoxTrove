2c47930aed5b8dbcb2145390a6b93eb5470c45df
Earlopain <earlopain@protonmail.com>
2021-09-10 23:59:24 +0200

    [Install] Add support for some image formats

--- vagrant/install/vips.sh
+++ vagrant/install/vips.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 VIPS_VERSION=$1
+
+apt-get install liblcms2-dev libjpeg-turbo8-dev libpng-dev libwebp-dev
+
 cd /tmp
 wget -q https://github.com/libvips/libvips/releases/download/v$VIPS_VERSION/vips-$VIPS_VERSION.tar.gz
 tar xf vips-$VIPS_VERSION.tar.gz
