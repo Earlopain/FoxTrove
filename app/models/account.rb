379a28e0623f7ead3a1e07cb2ed485d5815a8964
Earlopain <earlopain@protonmail.com>
2021-09-12 18:52:36 +0200

    [DB] Add enums to model

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,16 @@
 class Account < ApplicationRecord
+  module Levels
+    UNACTIVATED = "unactivated".freeze
+    MEMBER = "member".freeze
+    ADMIN = "admin".freeze
+  end
+
+  module Permissions
+    DELETE_ARTIST = "delete_artist".freeze
+    REQUEST_MANUAL_UPDATE = "request_manual_update".freeze
+    ALLOW_URL_MODERATION = "allow_url_moderation".freeze
+  end
+
   has_many :artists, foreign_key: :creator_id
   has_many :artist_urls, foreign_key: :creator_id
   has_many :created_moderation_logs, foreign_key: :creator_id, class_name: "ModerationLog"
--- app/models/artist_submission.rb
+++ app/models/artist_submission.rb
@@ -1,4 +1,11 @@
 class ArtistSubmission < ApplicationRecord
+  module Extensions
+    PNG = "png".freeze
+    JPG = "jpg".freeze
+    GIF = "gif".freeze
+    WEBM = "webm".freeze
+  end
+
   belongs_to :artist_url
   has_one :site, through: :artist_url
   has_one :artist, through: :artist_url
