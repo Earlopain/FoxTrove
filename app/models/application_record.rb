ecf8a1b56d7d2f0ac7cb63f6c6e245c598e08795
Earlopain <earlopain@protonmail.com>
2021-09-12 23:38:40 +0200

    [Sites] Fill table when seeding

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -8,4 +8,12 @@ class ApplicationRecord < ActiveRecord::Base
       belongs_to :creator, class_name: "Account"
     end
   end
+
+  def self.update_or_create(attributes)
+    raise StandardError, "Tried to update_or_create more than one records" if count > 1
+
+    obj = first || new
+    obj.assign_attributes attributes
+    obj
+  end
 end
