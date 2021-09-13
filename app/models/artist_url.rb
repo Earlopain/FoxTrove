e6d20cb928bb16c5ec8a318973287bcb93f78cb5
Earlopain <earlopain@protonmail.com>
2021-09-13 22:53:37 +0200

    [Sites] Extract artist identifier and site from url

--- app/models/artist_url.rb
+++ app/models/artist_url.rb
@@ -4,4 +4,24 @@ class ArtistUrl < ApplicationRecord
   belongs_to :site
   belongs_to :artist
   has_many :submissions, class_name: "ArtistSubmission"
+
+  def self.parse(url)
+    site = get_matching_site url
+    return nil if site.nil?
+
+    matches = site.artist_url_identifier_regex.match url
+    # This shouldn't happen since we definatly already matched in get_matching_site
+    return nil if matches[:artist_identifier].nil?
+
+    {
+      site: site,
+      artist_identifier: matches[:artist_identifier],
+    }
+  end
+
+  def self.get_matching_site(url)
+    Site.find_each.filter do |site|
+      url =~ site.artist_url_identifier_regex
+    end.first
+  end
 end
