b06d526c21566a88e6dd065c511aa27d8f89c5d1
Earlopain <earlopain@protonmail.com>
2021-12-15 16:02:38 +0100

    [Misc] Load Rails 7 defaults

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ Bundler.require(*Rails.groups)
 module Reverser
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
     config.active_record.schema_format = :sql
 
     config.autoload_paths += %W[#{config.root}/app/logical]
