8bdd6aa98fd6b1f5b6194863a8d24c50ec32ba24
Earlopain <earlopain@protonmail.com>
2021-12-17 23:43:37 +0100

    [Submissions] Remove iqdb thumb variant
    iqdb expects thumbs to be square but this respected aspect ratio

--- app/models/artist_submission.rb
+++ app/models/artist_submission.rb
@@ -3,7 +3,6 @@ class ArtistSubmission < ApplicationRecord
   has_one :site, through: :artist_url
   has_one :artist, through: :artist_url
   has_one_attached :file do |attachable|
-    attachable.variant :iqdb_thumb, resize_to_limit: [128, 128], format: :jpg
     attachable.variant :thumb, resize_to_limit: [Reverser.thumbnail_size, Reverser.thumbnail_size], format: :jpg
   end
 
