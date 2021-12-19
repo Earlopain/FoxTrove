a442fa9fd9915b5559f3dff07c2f72ffd3864d26
Earlopain <earlopain@protonmail.com>
2021-12-19 16:48:10 +0100

    Fix web-console

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.web_console.permissions = "192.168.0.0/16"
+  config.web_console.permissions = "0.0.0.0/0"
 
   config.active_storage.service = :local
 end
