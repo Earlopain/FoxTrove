087c0ea3c535a1456454275f22aa50e25dd4d6c0
Earlopain <earlopain@protonmail.com>
2021-12-28 16:53:06 +0100

    Improve icon embedding

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,13 @@ module ApplicationHelper
   def time_ago(value)
     tag.span value.to_formatted_s(:long), datetime: value.to_formatted_s(:iso8601), class: "time-ago"
   end
+
+  def site_icon(artist_url, text: nil, link_target: nil, **options)
+    icon = tag.span(class: artist_url.site.icon_class)
+    icon.concat tag.span(text, class: "site-icon-text") if text
+
+    return tag.span(icon, **options) unless link_target
+
+    link_to tag.span(icon), link_target, options
+  end
 end
