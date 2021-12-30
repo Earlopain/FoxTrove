99225b7a47d55232c46102d7b447405db6a76347
Earlopain <earlopain@protonmail.com>
2021-12-30 21:03:29 +0100

    Generate git url dynamically

--- app/logical/git_helper.rb
+++ app/logical/git_helper.rb
@@ -1,7 +1,20 @@
 module GitHelper
-  def self.git_hash
-    return @git_hash if instance_variable_defined? :@git_hash
+  def self.hash
+    return @hash if instance_variable_defined? :@hash
 
-    @git_hash = `git rev-parse --short HEAD`.strip if system("git rev-parse --show-toplevel", %i[out err] => File::NULL)
+    @hash = `git rev-parse --short HEAD`.strip
+  end
+
+  def self.url
+    return @url if instance_variable_defined? :@url
+
+    regex = %r{git@(\S*):(\S*)/(\S*)\.git|://(\S*)\.git}
+    remote = `git config --get remote.origin.url`.strip
+    match = remote.scan(regex).first
+    @url = if match[0]
+             "https://#{match[0]}/#{match[1]}/#{match[2]}"
+           else
+             "https://#{match[4]}"
+           end
   end
 end
