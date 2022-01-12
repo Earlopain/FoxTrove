cfa07f0d6fba68c3be4eb915e92251bc56a00bc5
Earlopain <earlopain@protonmail.com>
2022-01-12 19:39:02 +0100

    Fix download of uri with cyrilic and other non ascii chars

--- app/logical/sites.rb
+++ app/logical/sites.rb
@@ -26,7 +26,7 @@ module Sites
   def download_file(outfile, uri, definition = nil)
     definition ||= for_domain(uri.domain)
     headers = definition&.download_headers || {}
-    response = HTTParty.get(uri, { headers: headers }) do |chunk|
+    response = HTTParty.get(uri.display_uri, { headers: headers }) do |chunk|
       outfile.write(chunk)
     end
     outfile.rewind
