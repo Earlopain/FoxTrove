c06522757cdd69435a939dbdb7f98d0a4327dfb9
Earlopain <earlopain@protonmail.com>
2022-01-18 21:28:39 +0100

    Add a few more stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,5 +1,13 @@
 class StatsController < ApplicationController
   def index
     @artist_urls = ArtistUrl.where id: SidekiqStats.active_urls
+    @original_size = sum_for("original", "SubmissionFile")
+    @sample_size = sum_for("sample", "SubmissionFile")
+  end
+
+  private
+
+  def sum_for(name, record_type)
+    ActiveStorage::Blob.joins(:attachments).where(attachments: { name: , record_type: }).sum(:byte_size)
   end
 end
