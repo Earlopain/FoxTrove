f09908b2e11b343a0a801d467171055039f71e52
Earlopain <earlopain@protonmail.com>
2022-01-19 17:58:48 +0100

    Add database usage to stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -3,6 +3,8 @@ class StatsController < ApplicationController
     @artist_urls = ArtistUrl.where id: SidekiqStats.active_urls
     @original_size = sum_for("original", "SubmissionFile")
     @sample_size = sum_for("sample", "SubmissionFile")
+    db_name = Rails.configuration.database_configuration[Rails.env]["database"]
+    @db_size = ActiveRecord::Base.connection.execute("SELECT pg_database_size('#{db_name}');").first["pg_database_size"]
   end
 
   private
