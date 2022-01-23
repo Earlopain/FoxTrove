ed6574bdfa67148922708a668dacae48dfa6bf38
Earlopain <earlopain@protonmail.com>
2022-01-23 16:23:47 +0100

    Add a few more stats to the artist index page

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,6 +2,7 @@ class Artist < ApplicationRecord
   belongs_to_creator
   has_many :artist_urls, dependent: :destroy
   has_many :submissions, through: :artist_urls
+  has_many :submission_files, through: :submissions
 
   validates :name, uniqueness: { case_sensitive: false }
   validates :name, printable_string: true
