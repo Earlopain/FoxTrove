4f21bf238b5551c4a43855ffcd90f0c165cc4d17
Earlopain <earlopain@protonmail.com>
2022-03-01 20:03:36 +0100

    Allow collecting perf events through selenium

--- app/logical/selenium_wrapper.rb
+++ app/logical/selenium_wrapper.rb
@@ -1,11 +1,13 @@
 class SeleniumWrapper
-  def self.driver
+  def self.driver(with_performance: false)
     options = Selenium::WebDriver::Chrome::Options.new
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_option("excludeSwitches", ["enable-automation"])
     options.add_option("useAutomationExtension", false)
 
-    cps = Selenium::WebDriver::Remote::Capabilities.chrome "goog:loggingPrefs": {}
+    prefs = {}
+    prefs[:performance] = "ALL" if with_performance
+    cps = Selenium::WebDriver::Remote::Capabilities.chrome "goog:loggingPrefs": prefs
 
     driver = Selenium::WebDriver.for :remote, capabilities: [options, cps], url: Config.selenium_url
     yield driver
