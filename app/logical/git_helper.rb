4aa2568093165158f1f82f5a260c603640de64e7
Earlopain <earlopain@protonmail.com>
2022-03-01 20:44:21 +0100

    Fix commit extraction for http cloned repos

--- app/logical/git_helper.rb
+++ app/logical/git_helper.rb
@@ -8,7 +8,7 @@ module GitHelper
   def self.url
     return @url if instance_variable_defined? :@url
 
-    regex = %r{git@(\S*):(\S*)/(\S*)\.git|://(\S*)\.git}
+    regex = %r{git@(\S*):(\S*)/(\S*)\.git|://(\S*)(\.git)?}
     remote = `git config --get remote.origin.url`.strip
     match = remote.scan(regex).first
     @url = if match[0]
