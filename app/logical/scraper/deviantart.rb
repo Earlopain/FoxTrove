1eab7b066053a54f0b049f245e3189522cf77bc4
Earlopain <earlopain@protonmail.com>
2022-04-22 08:04:09 +0200

    Use better quality on deviantart when no direct download

--- app/logical/scraper/deviantart.rb
+++ app/logical/scraper/deviantart.rb
@@ -35,7 +35,7 @@ module Scraper
       s.created_at = created_at
       s.add_file({}.tap do |hash|
         hash[:url_data] = [submission["deviationid"]] if submission["is_downloadable"]
-        hash[:url] = submission["content"]["src"] unless submission["is_downloadable"]
+        hash[:url] = submission["content"]["src"].sub("q_80,strp", "q_100") unless submission["is_downloadable"]
         hash[:created_at] = created_at
         hash[:identifier] = ""
       end)
