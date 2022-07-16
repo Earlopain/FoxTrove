e3f338bbed38dc930d682d54cd9abea62cea4033
Earlopain <earlopain@protonmail.com>
2022-07-17 00:05:46 +0200

    Remove gif thumbnail generation comment
    This doesn't actually generate an animated gif like I though.
    Also some variable name cleanup.

--- app/logical/variant_generator.rb
+++ app/logical/variant_generator.rb
@@ -1,39 +1,27 @@
 module VariantGenerator
   def self.sample(attachment)
-    path = ActiveStorage::Blob.service.path_for(attachment.key)
-    file = Tempfile.new
+    input_path = ActiveStorage::Blob.service.path_for(attachment.key)
+    output_file = Tempfile.new
     # TODO: Error handling
     case attachment.content_type
-      # FIXME: alpine vips is currrently not build with cgif, saving animated gifs is not possible
     when "image/jpeg", "image/png", "image/webp", "image/gif"
-      image_thumb(path, file, Config.thumbnail_size, height: Config.thumbnail_size, size: :down)
-    when "image/gif"
-      gif_thumb(path, file, Config.thumbnail_size, height: Config.thumbnail_size, size: :down)
+      image = Vips::Image.thumbnail(input_path, Config.thumbnail_size, height: Config.thumbnail_size, size: :down)
+      image.jpegsave(output_file.path, Q: 90)
     when "video/mp4"
-      params = mp4_conversion_parameters(path, file.path, [Config.thumbnail_size, Config.thumbnail_size])
+      params = mp4_conversion_parameters(input_path, output_file.path, [Config.thumbnail_size, Config.thumbnail_size])
       stdout, stderr, status = Open3.capture3("/usr/bin/ffmpeg", *params)
-      raise StandardError, "unable to transcode files\n#{stdout.chomp}\n\n#{stderr.chomp}" if status != 0
+      raise StandardError, "Unable to transcode file\n#{stdout.chomp}\n\n#{stderr.chomp}" if status != 0
     else
       raise StandardError, "Unhandled content_type #{attachment.content_type}"
     end
-    file
+    output_file
   end
 
-  def self.image_thumb(input_path, outfile, width, **options)
-    image = Vips::Image.thumbnail(input_path, width, **options)
-    image.jpegsave(outfile.path, Q: 90)
-  end
-
-  def self.gif_thumb(input_path, outfile, width, **options)
-    image = Vips::Image.thumbnail(input_path, width, **options)
-    image.gifsave(outfile.path)
-  end
-
-  def self.mp4_conversion_parameters(in_path, out_path, dimensions)
+  def self.mp4_conversion_parameters(input_path, output_path, dimensions)
     target_size = "scale=w=#{dimensions[0]}:h=#{dimensions[1]}:force_original_aspect_ratio=decrease,pad=width=ceil(iw/2)*2:height=ceil(ih/2)*2"
     [
       "-i",
-      in_path,
+      input_path,
       "-vf",
       target_size,
       "-y",
@@ -63,7 +51,7 @@ module VariantGenerator
       "+faststart",
       "-f",
       "mp4",
-      out_path,
+      output_path,
     ]
   end
 end
