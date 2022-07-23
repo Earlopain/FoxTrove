71c596dbbe383b440af5cafcd41f09fd3785bfa0
Earlopain <earlopain@protonmail.com>
2022-07-23 17:55:22 +0200

    Add tests for url fixing

--- app/logical/sites.rb
+++ app/logical/sites.rb
@@ -26,27 +26,25 @@ module Sites
   end
 
   def download_file(outfile, url, definition = nil)
-    # produces the right uri for all of these:
-    # https://d.furaffinity.net/art/peyzazhik/1629082282/1629082282.peyzazhik_%D0%B7%D0%B0%D0%BB%D0%B8%D0%B2%D0%B0%D1%82%D1%8C-%D0%B3%D0%B8%D1%82%D0%B0%D1%80%D1%83.jpg
-    # https://d.furaffinity.net/art/peyzazhik/1629082282/1629082282.peyzazhik_заливать-гитару.jpg
-    # https://d.furaffinity.net/art/nawka/1642391380/1642391380.nawka__sd__kwaza_and_hector_[final].jpg
-    # https://d.furaffinity.net/art/fr95/1635001690/1635001679.fr95_co＠f-r9512.png  (notice the different @ sign)
-    unencoded = Addressable::URI.unencode(url)
-    escaped = Addressable::URI.escape(unencoded)
-    uri = Addressable::URI.parse(escaped)
-    # https://www.newgrounds.com/art/view/nawka/comm-soot-and-lunamew
-    # Secondary image url is missing the scheme, just assume https in those cases
-    uri.scheme = "https" unless uri.scheme
-    raise Addressable::URI::InvalidURIError, "scheme must be http(s)" unless uri.scheme.in?(%w[http https])
-    raise Addressable::URI::InvalidURIError, "host must be set" if uri.host.blank?
-
+    fixed_uri = fix_url(url)
     definition ||= for_domain(uri.domain)
     headers = definition&.download_headers || {}
-    response = HTTParty.get(uri, { headers: headers }) do |chunk|
+    response = HTTParty.get(fixed_uri, { headers: headers }) do |chunk|
       outfile.write(chunk)
     end
     outfile.rewind
     response
+  end
+
+  def fix_url(url)
+    unencoded = Addressable::URI.unencode(url)
+    escaped = Addressable::URI.escape(unencoded)
+    uri = Addressable::URI.parse(escaped)
+    uri.scheme = "https" unless uri.scheme
+    raise Addressable::URI::InvalidURIError, "scheme must be http(s)" unless uri.scheme.in?(%w[http https])
+    raise Addressable::URI::InvalidURIError, "host must be set" if uri.host.blank?
+
+    uri
   end
 
   ALL = Definitions.constants.map { |name| Definitions.const_get(name).new }
---
+++ spec/logical/sites_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+RSpec.describe Sites do
+  it "returns the correct definition for enum lookups" do
+    expect(described_class.from_enum("twitter")).to be_a Sites::Definitions::Twitter
+    expect(described_class.from_enum("twitch")).to be_a Sites::Definitions::Twitch
+  end
+
+  describe "fix_url" do
+    it "correctly escapes cyrilic characters" do
+      input = "https://d.furaffinity.net/art/peyzazhik/1629082282/1629082282.peyzazhik_заливать-гитару.jpg"
+      output = "https://d.furaffinity.net/art/peyzazhik/1629082282/1629082282.peyzazhik_%D0%B7%D0%B0%D0%BB%D0%B8%D0%B2%D0%B0%D1%82%D1%8C-%D0%B3%D0%B8%D1%82%D0%B0%D1%80%D1%83.jpg"
+      expect(described_class.fix_url(input).to_s).to eq(output)
+    end
+
+    it "correctly escapes square brackets" do
+      input = "https://d.furaffinity.net/art/nawka/1642391380/1642391380.nawka__sd__kwaza_and_hector_[final].jpg"
+      output = "https://d.furaffinity.net/art/nawka/1642391380/1642391380.nawka__sd__kwaza_and_hector_%5Bfinal%5D.jpg"
+      expect(described_class.fix_url(input).to_s).to eq(output)
+    end
+
+    it "correctly escapes ＠" do
+      input = "https://d.furaffinity.net/art/fr95/1635001690/1635001679.fr95_co＠f-r9512.png"
+      output = "https://d.furaffinity.net/art/fr95/1635001690/1635001679.fr95_co%EF%BC%A0f-r9512.png"
+      expect(described_class.fix_url(input).to_s).to eq(output)
+    end
+
+    it "assumes https when no scheme is present" do
+      input  = "//art.ngfiles.com/comments/2000/iu_2391_7119353.jpg"
+      output = "https://art.ngfiles.com/comments/2000/iu_2391_7119353.jpg"
+      expect(described_class.fix_url(input).to_s).to eq(output)
+    end
+  end
+end
