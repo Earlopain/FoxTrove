263d9d1fc9f82f66704bec7d745a1999fb0130d4
Earlopain <earlopain@protonmail.com>
2022-08-02 21:10:59 +0200

    Add direct e6 image link for quick opening
    Also increase the submission width, since I am running out of space

--- app/models/e6_iqdb_data.rb
+++ app/models/e6_iqdb_data.rb
@@ -2,4 +2,16 @@
 
 class E6IqdbData < ApplicationRecord
   belongs_to :submission_file
+
+  def direct_url
+    post_json["file"]["url"]
+  end
+
+  def score
+    post_json["score"]["total"]
+  end
+
+  def deleted?
+    post_json["flags"]["deleted"]
+  end
 end
