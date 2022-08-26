e629ebdc3696ca97843a9a84cb74851814b47600
Earlopain <earlopain@protonmail.com>
2022-08-26 22:04:50 +0200

    Order and put scrapable artist urls at the top
    This gives a nice deterministic order for every artist, regardless of
    the order of when they added initially

--- app/helpers/artist_url_helper.rb
+++ app/helpers/artist_url_helper.rb
@@ -17,4 +17,12 @@ module ArtistUrlHelper
     icon = tag.span(class: artist_url.site.icon_class)
     link_to tag.span(icon), gallery_url(artist_url), title: "#{artist_url.site.display_name} - #{artist_url.url_identifier}"
   end
+
+  def ordered_artist_urls(artist)
+    artist.artist_urls.sort_by do |artist_url|
+      numeric_site_type = artist_url.class.site_types[artist_url.site_type]
+      scraper_modifier = artist_url.scraper_enabled? ? 0 : 100
+      numeric_site_type + scraper_modifier
+    end
+  end
 end
