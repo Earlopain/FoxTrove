3ba67eb9c3a2fc8cff3c4272cbdcfcc3ae321ab4
Earlopain <earlopain@protonmail.com>
2022-09-05 19:59:10 +0200

    Remove primary/secondary partials
    Not necessary, adds logspam. Also remove unused ids/classes.

--- app/helpers/nav_helper.rb
+++ app/helpers/nav_helper.rb
@@ -2,17 +2,17 @@
 
 module NavHelper
   def nav_link_to(text, url, **options)
-    render "application/primary_link", text: text, url: url, options: options
+    link = link_to(text, url, class: nav_link_class(url), **options)
+    tag.li(link)
   end
 
   def subnav_link_to(text, url, **options)
-    render "application/secondary_link", text: text, url: url, options: options
+    link = link_to(text, url, **options)
+    tag.li(link)
   end
 
   def nav_link_class(url)
     return "current" if url.start_with?("/#{params[:controller]}") || url.start_with?("/#{params[:controller].singularize}")
     return "current" if url == "/" && request.path == "/"
-
-    ""
   end
 end
