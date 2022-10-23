1a8bed0fb8d28b0f53a70f51926b111d0ab8c030
Earlopain <earlopain+github@protonmail.com>
2022-10-23 16:10:24 +0200

    Use jpg thumb for videos
    Makes selecting them possible

--- app/helpers/submission_file_helper.rb
+++ app/helpers/submission_file_helper.rb
@@ -6,4 +6,13 @@ module SubmissionFileHelper
     s2 = submission_file.artist.name.downcase
     s1.gsub(/\W+/, "") != s2.gsub(/\W+/, "")
   end
+
+  def original_file_tag(submission_file)
+    url = url_for(submission_file.original)
+    if submission_file.original.content_type == "video/mp4"
+      video_tag url, controls: true, class: "submission-file-full hidden"
+    else
+      image_tag url, loading: "lazy", class: "submission-file-full hidden"
+    end
+  end
 end
--- app/logical/variant_generator.rb
+++ app/logical/variant_generator.rb
@@ -3,57 +3,19 @@
 module VariantGenerator
   def self.sample(attachment)
     input_path = ActiveStorage::Blob.service.path_for(attachment.key)
-    output_file = Tempfile.new
+    output_file = Tempfile.new(["", ".jpg"])
     # TODO: Error handling
     case attachment.content_type
     when "image/jpeg", "image/png", "image/webp", "image/gif"
       image = Vips::Image.thumbnail(input_path, Config.thumbnail_size, height: Config.thumbnail_size, size: :down)
       image.jpegsave(output_file.path, Q: 90)
     when "video/mp4"
-      params = mp4_conversion_parameters(input_path, output_file.path, [Config.thumbnail_size, Config.thumbnail_size])
-      stdout, stderr, status = Open3.capture3("/usr/bin/ffmpeg", *params)
-      raise StandardError, "Unable to transcode file\n#{stdout.chomp}\n\n#{stderr.chomp}" if status != 0
+      target_size = "thumbnail,scale=w=#{Config.thumbnail_size}:h=#{Config.thumbnail_size}:force_original_aspect_ratio=decrease,pad=width=ceil(iw/2)*2:height=ceil(ih/2)*2"
+      stdout, stderr, status = Open3.capture3("/usr/bin/ffmpeg", "-y", "-i", input_path, "-vf", target_size, "-frames:v", "1", output_file.path)
+      raise StandardError, "Unable to thumbnail file\n#{stdout.chomp}\n\n#{stderr.chomp}" if status != 0
     else
       raise StandardError, "Unhandled content_type #{attachment.content_type}"
     end
     output_file
-  end
-
-  def self.mp4_conversion_parameters(input_path, output_path, dimensions)
-    target_size = "scale=w=#{dimensions[0]}:h=#{dimensions[1]}:force_original_aspect_ratio=decrease,pad=width=ceil(iw/2)*2:height=ceil(ih/2)*2"
-    [
-      "-i",
-      input_path,
-      "-vf",
-      target_size,
-      "-y",
-      "-c:v",
-      "libx264",
-      "-pix_fmt",
-      "yuv420p",
-      "-profile:v",
-      "main",
-      "-preset",
-      "fast",
-      "-crf",
-      "27",
-      "-b:v",
-      "3M",
-      "-threads",
-      "4",
-      "-max_muxing_queue_size",
-      "4096",
-      "-c:a",
-      "aac",
-      "-b:a",
-      "128k",
-      "-map_metadata",
-      "-1",
-      "-movflags",
-      "+faststart",
-      "-f",
-      "mp4",
-      output_path,
-    ]
   end
 end
