10511a45300baa0a86cacf15667d3b0805721acf
Earlopain <earlopain+github@protonmail.com>
2022-10-23 18:11:09 +0200

    Fix missing search button
    Turns out I need both yields

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -45,7 +45,7 @@ module ApplicationHelper
   def hideable_search(path, &)
     search = toggleable("Show Search Options", "Hide Search Options", visible_on_load: params[:search].present?) do
       simple_form_for(:search, method: :get, url: path, defaults: { required: false }, builder: HideableSearchFormBuilder, search_params: params[:search]) do |f|
-        yield(f) + f.submit("Search")
+        capture { yield(f) } + f.submit("Search")
       end
     end
     tag.div(search)
