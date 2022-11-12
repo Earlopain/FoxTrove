Earlopain/FoxTrove

commit 22bf21f9f3a2c69de72ce9ecf1d49631fd07c2f6

Unify scraper HTTParty access

app/logical/scraper/base.rb
@@ -53,5 +53,27 @@ module Scraper
     def fetch_api_identifier
       raise NotImplementedError
     end
+
+    def fetch_html(path, method = :get, **params)
+      HTTParty.send(method, path, **params)
+    end
+
+    def fetch_json(path, method = :get, **params)
+      response = HTTParty.send(method, path, **params)
+      # Validate that the response is indeed json
+      JSON.parse(response.body)
+      response
+    end
+
+    def fetch_json_selenium(path)
+      SeleniumWrapper.driver do |d|
+        d.navigate.to path
+        begin
+          JSON.parse(d.find_element(css: "pre").text)
+        rescue Selenium::WebDriver::Error::NoSuchElementError
+          raise JSON::ParserError, "#{path}: No response"
+        end
+      end
+    end
   end
 end

app/logical/scraper/artstation.rb
@@ -36,19 +36,19 @@ module Scraper
 
     def fetch_api_identifier
       response = make_request("/users/#{url_identifier}/quick.json")
-      JSON.parse(response.body)["id"]
+      response["id"]
     rescue JSON::ParserError
       nil
     end
 
     def get_ids_from_page(page)
       response = make_request("/users/#{url_identifier}/projects.json?page=#{page}")
-      JSON.parse(response)["data"].map { |entry| entry["hash_id"] }
+      response["data"].map { |entry| entry["hash_id"] }
     end
 
     def get_details(ids)
       details = ids.map do |id|
-        JSON.parse(make_request("/projects/#{id}.json"))
+        make_request("/projects/#{id}.json")
       end
       # Remove any non-image assets
       details.map do |entry|
@@ -60,18 +60,9 @@ module Scraper
       end
     end
 
-    # Provide an option driver instance when possible,
-    # to reduce selenium startup time
+    # FIXME: Figure out a way to do this without selenium
     def make_request(path)
-      # FIXME: Figure out a way to do this without selenium
-      SeleniumWrapper.driver do |d|
-        d.navigate.to "https://www.artstation.com#{path}"
-        begin
-          d.find_element(css: "pre").text
-        rescue Selenium::WebDriver::Error::NoSuchElementError
-          ""
-        end
-      end
+      fetch_json_selenium("https://www.artstation.com#{path}")
     end
   end
 end

app/logical/scraper/deviantart.rb
@@ -57,7 +57,7 @@ module Scraper
     private
 
     def make_api_call(endpoint, query = {})
-      response = HTTParty.get("#{API_PREFIX}/#{endpoint}", {
+      fetch_json("#{API_PREFIX}/#{endpoint}", **{
         query: {
           access_token: access_token,
           **query,
@@ -66,17 +66,16 @@ module Scraper
           "dA-minor-version": "20210526",
         },
       })
-      JSON.parse(response.body)
     end
 
     def access_token
       Cache.fetch("deviantart-token", 55.minutes) do
-        response = HTTParty.get("https://www.deviantart.com/oauth2/token", query: {
+        response = fetch_json("https://www.deviantart.com/oauth2/token", query: {
           grant_type: "client_credentials",
           client_id: Config.deviantart_client_id,
           client_secret: Config.deviantart_client_secret,
         })
-        JSON.parse(response.body)["access_token"]
+        response["access_token"]
       end
     end
   end

app/logical/scraper/furaffinity.rb
@@ -65,8 +65,7 @@ module Scraper
     private
 
     def get_submission_ids(page)
-      url = "https://www.furaffinity.net/search"
-      response = HTTParty.post(url, {
+      response = fetch_html("https://www.furaffinity.net/search", :post, **{
         headers: headers,
         body: {
           "page": page,
@@ -94,8 +93,7 @@ module Scraper
     end
 
     def get_submission_html(id)
-      url = "https://www.furaffinity.net/view/#{id}"
-      response = HTTParty.get(url, { headers: headers })
+      response = fetch_html("https://www.furaffinity.net/view/#{id}", headers: headers)
       Nokogiri::HTML(response.body)
     end
 

app/logical/scraper/furrynetwork.rb
@@ -48,13 +48,12 @@ module Scraper
     private
 
     def make_request(endpoint, query = {})
-      response = HTTParty.get("#{API_PREFIX}/#{endpoint}", {
+      fetch_json("#{API_PREFIX}/#{endpoint}", **{
         query: query,
         headers: {
           Authorization: "Bearer #{bearer_token}",
         },
       })
-      JSON.parse(response.body)
     end
 
     # This whole thing is very brittle and may break at any moment

app/logical/scraper/inkbunny.rb
@@ -87,7 +87,7 @@ module Scraper
     end
 
     def make_request(url, query_params)
-      HTTParty.get(url, query: {
+      fetch_json(url, query: {
         **query_params,
         sid: @sid,
       })

app/logical/scraper/mastodon_v1.rb
@@ -55,7 +55,7 @@ module Scraper
 
     def make_request(path, **query_params)
       url = "https://#{domain}/api/v1#{path}"
-      HTTParty.get(url, query: query_params, headers: { Authorization: "Bearer #{access_token}" })
+      fetch_json(url, query: query_params, headers: { Authorization: "Bearer #{access_token}" })
     end
   end
 end

app/logical/scraper/newgrounds.rb
@@ -49,7 +49,7 @@ module Scraper
     end
 
     def fetch_api_identifier
-      response = HTTParty.get("https://#{url_identifier}.newgrounds.com/")
+      response = fetch_html("https://#{url_identifier}.newgrounds.com/")
       html = Nokogiri::HTML(response.body)
       html.at("#topsearch-elastic input[name='u']")&.attribute("value")&.value
     end
@@ -58,7 +58,7 @@ module Scraper
 
     def get_from_page(page)
       url = "https://#{url_identifier}.newgrounds.com/art/page/#{page}"
-      response = HTTParty.get(url, headers: {
+      response = fetch_json(url, headers: {
         "X-Requested-With": "XMLHttpRequest",
         "Cookie": "#{COOKIE_NAME}=#{fetch_cookie}",
       })
@@ -66,7 +66,7 @@ module Scraper
     end
 
     def get_submission_details(url)
-      response = HTTParty.get(url, headers: { Cookie: "#{COOKIE_NAME}=#{fetch_cookie}" })
+      response = fetch_html(url, headers: { Cookie: "#{COOKIE_NAME}=#{fetch_cookie}" })
       html = Nokogiri::HTML(response.body)
       media_object = html.at("[itemtype='https://schema.org/MediaObject']")
       main_image_url = media_object.at(".image img").attributes["src"].value

commit 6888a2262e759fdd49e406fb171e12c44c1bcaf3

Fix sofurry scraper
Likely broken by 22bf21f9f3a2c69de72ce9ecf1d49631fd07c2f6.
HTTParty uses the content type to determine the response class.
Sofurry continues to  provide an amazing API and doesn't return application/json

app/logical/scraper/base.rb
@@ -91,11 +91,9 @@ module Scraper
 
     def fetch_json(path, method = :get, **params)
       response = enfore_rate_limit do
-        HTTParty.send(method, path, params)
+        HTTParty.send(method, path, { format: :json, **params })
       end
       log_response(path, method, params, response.code, response.body)
-      # Validate that the response is indeed json
-      JSON.parse(response.body)
       response
     end
 