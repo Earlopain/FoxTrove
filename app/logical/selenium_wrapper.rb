5749c7070df6428f16701bd4823181307206e2b2
Earlopain <earlopain@protonmail.com>
2023-04-19 21:19:59 +0200

    Resolve selenium deprecations

--- app/logical/selenium_wrapper.rb
+++ app/logical/selenium_wrapper.rb
@@ -2,13 +2,12 @@
 
 class SeleniumWrapper
   def self.driver(with_performance: false)
-    options = Selenium::WebDriver::Chrome::Options.new
+    options = Selenium::WebDriver::Chrome::Options.new(exclude_switches: ["enable-automation"])
     options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_option("excludeSwitches", ["enable-automation"])
 
     prefs = {}
     prefs[:performance] = "ALL" if with_performance
-    cps = Selenium::WebDriver::Remote::Capabilities.chrome "goog:loggingPrefs": prefs
+    cps = Selenium::WebDriver::Options.chrome "goog:loggingPrefs": prefs
 
     driver = Selenium::WebDriver.for :remote, capabilities: [options, cps], url: Config.selenium_url
     if block_given?
