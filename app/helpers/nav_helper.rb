3b211ada8c599d6ed9371831f15bbb4422d406b2
Earlopain <14981592+Earlopain@users.noreply.github.com>
2023-09-29 16:41:23 +0200

    Always render secondary links as full height

--- app/helpers/nav_helper.rb
+++ app/helpers/nav_helper.rb
@@ -11,6 +11,10 @@ module NavHelper
     tag.li(link)
   end
 
+  def secondary_links_exist?
+    lookup_context.template_exists?("secondary_links", controller_name, true)
+  end
+
   def nav_link_class(url)
     return "current" if url == "/#{params[:controller]}" || url.start_with?("/#{params[:controller]}/")
 
