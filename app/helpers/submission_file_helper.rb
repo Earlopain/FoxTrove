42bc6deb49278ec5d378adcd8884865888b9ddee
Earlopain <14981592+Earlopain@users.noreply.github.com>
2023-10-03 20:59:45 +0200

    Also don't relayout for the full view image
    Same as the previous commit. Didn't seem to want to work but I randomly
    figured out the right combination of css, like I usually do

--- app/helpers/submission_file_helper.rb
+++ app/helpers/submission_file_helper.rb
@@ -7,12 +7,19 @@ module SubmissionFileHelper
     s1.gsub(/\W+/, "") != s2.gsub(/\W+/, "")
   end
 
+  def submission_file_tag(submission_file)
+    dimensions = { width: submission_file.width, height: submission_file.height }
+    sample = image_tag(url_for(submission_file.sample), loading: "lazy", class: "submission-file", **dimensions)
+    sample + original_file_tag(submission_file)
+  end
+
   def original_file_tag(submission_file)
     url = url_for(submission_file.original)
+    dimensions = { width: submission_file.width, height: submission_file.height }
     if submission_file.original.content_type.in? ["video/mp4", "video/webm"]
-      video_tag url, controls: false, class: "submission-file-full hidden"
+      video_tag(url, controls: false, class: "submission-file-full hidden", **dimensions)
     else
-      image_tag url, loading: "lazy", class: "submission-file-full hidden"
+      image_tag(url, loading: "lazy", class: "submission-file-full hidden", **dimensions)
     end
   end
 end
