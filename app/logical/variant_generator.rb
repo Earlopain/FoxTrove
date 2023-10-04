c9bd4450e56547a9976c334bdb13dd51f02f86e3
Earlopain <14981592+Earlopain@users.noreply.github.com>
2023-10-04 15:10:00 +0200

    Remove thumbnail size config

--- app/logical/variant_generator.rb
+++ app/logical/variant_generator.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
 module VariantGenerator
+  THUMBNAIL_SIZE = 300
+
   def self.sample(input_path, content_type)
     output_file = Tempfile.new(["", ".jpg"])
     # TODO: Error handling
     case content_type
     when "image/jpeg", "image/png", "image/webp", "image/gif"
-      image = Vips::Image.thumbnail(input_path, Config.thumbnail_size, height: Config.thumbnail_size, size: :down)
+      image = Vips::Image.thumbnail(input_path, THUMBNAIL_SIZE, height: THUMBNAIL_SIZE, size: :down)
       image.jpegsave(output_file.path, Q: 90)
     when "video/mp4", "video/webm"
-      target_size = "thumbnail,scale=w=#{Config.thumbnail_size}:h=#{Config.thumbnail_size}:force_original_aspect_ratio=decrease,pad=width=ceil(iw/2)*2:height=ceil(ih/2)*2"
+      target_size = "thumbnail,scale=w=#{THUMBNAIL_SIZE}:h=#{THUMBNAIL_SIZE}:force_original_aspect_ratio=decrease,pad=width=ceil(iw/2)*2:height=ceil(ih/2)*2"
       stdout, stderr, status = Open3.capture3("/usr/bin/ffmpeg", "-y", "-i", input_path, "-vf", target_size, "-frames:v", "1", output_file.path)
       raise StandardError, "Unable to thumbnail file\n#{stdout.chomp}\n\n#{stderr.chomp}" if status != 0
     else
