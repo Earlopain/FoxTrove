16cea1229ca79f0cf420c8581eb1319a4ebff6d3
Earlopain <14981592+Earlopain@users.noreply.github.com>
2024-05-12 12:47:39 +0200

    Remove some redundant info in the multiselect
    It was getting awfully long

--- app/typescript/multiselect_mode.ts
+++ app/typescript/multiselect_mode.ts
@@ -3,10 +3,10 @@ import Toggleable from "./toggleable";
 
 export default class MultiselectMode {
   private static submitting = false;
-  private static counters: NodeListOf<HTMLElement>;
+  private static counter: HTMLElement | null;
 
   public static init() {
-    this.counters = document.querySelectorAll(".selected-count");
+    this.counter = document.getElementById("selected-count");
     
     const toggleable = Toggleable.get("multiselect");
     toggleable?.setShowAction(() => {
@@ -62,9 +62,9 @@ export default class MultiselectMode {
   }
 
   private static setCount(count?: number) {
-    count = count || ClickMode.getSelectedIds().length;
-    for (const counter  of this.counters) {
-      counter.innerText = count.toString();
+    if(this.counter) {
+      count = count || ClickMode.getSelectedIds().length;
+      this.counter.innerText = count.toString();
     }
   }
 
