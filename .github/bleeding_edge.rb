831f8e046f5d895b8dd3306d6e82d1dd11414570
Earlopain <14981592+Earlopain@users.noreply.github.com>
2024-10-21 16:16:39 +0200

    Update bleeding_edge.rb

--- .github/bleeding_edge.rb
+++ .github/bleeding_edge.rb
@@ -3,7 +3,7 @@ lines = ["source \"https://rubygems.org\""]
 Gem::Specification.stubs.each do |stub|
   url = stub.metadata["source_code_uri"] || stub.homepage
   matched = url[%r{https?://git(?:hub|lab)\.com/[^\/]*/[^\/]*}]
-  if matched && stub.name != "minitest"
+  if matched && stub.name != "minitest" && stub.name != "i18n"
     lines << "gem #{stub.name.inspect}, git: #{matched.inspect}"
   else
     lines << "gem #{stub.name.inspect}"
