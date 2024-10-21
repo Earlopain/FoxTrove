27408c5818d614224cd38cde42e2028f14012ae8
Earlopain <14981592+Earlopain@users.noreply.github.com>
2024-10-21 15:55:27 +0200

    Bleeding edge workflow

---
+++ .github/bleeding_edge.rb
@@ -0,0 +1,13 @@
+lines = ["source \"https://rubygems.org\""]
+
+Gem::Specification.stubs.each do |stub|
+  url = stub.metadata["source_code_uri"] || stub.homepage
+  matched = url[%r{https?://git(?:hub|lab)\.com/[^\/]*/[^\/]*}]
+  if matched && stub.name != "minitest"
+    lines << "gem #{stub.name.inspect}, git: #{matched.inspect}"
+  else
+    lines << "gem #{stub.name.inspect}"
+  end
+end
+
+File.write("Gemfile", lines.join("\n"))
