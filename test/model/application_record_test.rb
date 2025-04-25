7bdb94b23f7186c556a569a0a9a3957f2f910861
Earlopain <14981592+Earlopain@users.noreply.github.com>
2025-04-25 09:50:14 +0200

    Fix some slightly flaky tests
    These queries don't have an explicit order attached to them,
    so postgres may return them in any order.
    
    I've seen one of the tests fail two times now. The order in which
    they are returned is not important, so just ignore it in the assertion.

--- test/model/application_record_test.rb
+++ test/model/application_record_test.rb
@@ -1,59 +1,63 @@
 require "test_helper"
 
 class ApplicationRecordTest < ActiveSupport::TestCase
+  def assert_equal_any_order(expected, actual)
+    assert_equal(expected.sort_by(&:id), actual.sort_by(&:id))
+  end
+
   test "when searching for an empty array it returns no results" do
     create(:submission_file, file_error: nil)
     create(:submission_file, file_error: "")
     create(:submission_file, file_error: "abc")
-    assert_equal(0, SubmissionFile.attribute_matches([], :file_error).count)
+    assert_empty(SubmissionFile.attribute_matches([], :file_error))
   end
 
   test "when searching for an empty string it returns correct results" do
     create(:submission_file, file_error: nil)
     result = create(:submission_file, file_error: "")
     create(:submission_file, file_error: "abc")
-    assert_equal(result.id, SubmissionFile.attribute_matches("", :file_error).sole.id)
+    assert_equal(result, SubmissionFile.attribute_matches("", :file_error).sole)
   end
 
   test "when searching by integer field" do
     sm1, _, sm3 = create_list(:submission_file, 3)
-    assert_equal([sm1, sm3], SubmissionFile.attribute_matches("#{sm1.id},#{sm3.id}", :id))
+    assert_equal_any_order([sm1, sm3], SubmissionFile.attribute_matches("#{sm1.id},#{sm3.id}", :id))
   end
 
   test "when searching by jsonb field" do
     e1 = create(:log_event, payload: { foo: "bar" })
     e2 = create(:log_event, payload: { baz: "bat" })
     e3 = create(:log_event, payload: { xyz: "123" })
-    assert_equal([e1, e2], LogEvent.attribute_matches("*b*", :payload))
-    assert_equal([e3], LogEvent.attribute_matches("*xyz*", :payload))
+    assert_equal_any_order([e1, e2], LogEvent.attribute_matches("*b*", :payload))
+    assert_equal_any_order([e3], LogEvent.attribute_matches("*xyz*", :payload))
   end
 
   test "when searching for nil it returns all results" do
     r1 = create(:submission_file, file_error: nil)
     r2 = create(:submission_file, file_error: "")
     r3 = create(:submission_file, file_error: "abc")
-    assert_equal([r1.id, r2.id, r3.id], SubmissionFile.attribute_matches(nil, :file_error).pluck(:id))
+    assert_equal_any_order([r1, r2, r3], SubmissionFile.attribute_matches(nil, :file_error))
   end
 
   test "when searching for nil values" do
     r1 = create(:submission_file, file_error: nil)
     r2 = create(:submission_file, file_error: "abc")
-    assert_equal(r1.id, SubmissionFile.attribute_nil_check(false, :file_error).sole.id)
-    assert_equal(r2.id, SubmissionFile.attribute_nil_check(true, :file_error).sole.id)
+    assert_equal(r1, SubmissionFile.attribute_nil_check(false, :file_error).sole)
+    assert_equal(r2, SubmissionFile.attribute_nil_check(true, :file_error).sole)
   end
 
   test "when searching for multiple values" do
     r1 = create(:artist, name: "foo")
     r2 = create(:artist, name: "bar")
     r3 = create(:artist, name: "faz")
-    assert_equal([r1.id, r2.id], Artist.attribute_matches("foo,bar", :name).pluck(:id))
-    assert_equal([r2.id, r3.id], Artist.attribute_matches("*a*", :name).pluck(:id))
-    assert_equal([r1.id, r2.id], Artist.attribute_matches("foo,*r", :name).pluck(:id))
+    assert_equal_any_order([r1, r2], Artist.attribute_matches("foo,bar", :name))
+    assert_equal_any_order([r2, r3], Artist.attribute_matches("*a*", :name))
+    assert_equal_any_order([r1, r2], Artist.attribute_matches("foo,*r", :name))
   end
 
   test "when searching by join attribute it returns the match only once" do
     result = create(:artist)
     create_list(:artist_url, 2, artist: result, site_type: "twitter")
-    assert_equal(result.id, Artist.join_attribute_matches("twitter", artist_urls: :site_type).sole.id)
+    assert_equal(result, Artist.join_attribute_matches("twitter", artist_urls: :site_type).sole)
   end
 end
