2e34aa9777db6937ce3c3e6894fa0455e9506112
faucet <102884856+faucetlol@users.noreply.github.com>
2025-04-28 09:58:09 +0100

    Don't discard submissions from users with display names

--- app/logical/scraper/furaffinity.rb
+++ app/logical/scraper/furaffinity.rb
@@ -79,8 +79,8 @@ module Scraper
       )
       # Searching for "@lower scale" returns results from blue-scale
       relevant_submissions = html.css("#browse-search figure").select do |element|
-        # Remove _ from displayname, https://www.furaffinity.net/user/thesecretcave/ => The_Secret_Cave
-        element.css("figcaption a")[1].content.downcase.delete("_") == url_identifier.downcase
+        # Display names can now be arbitrary text, so take the identifier from the href.
+        element.css("figcaption a")[1]["href"].split("/")[2] == url_identifier.downcase
       end
       relevant_submissions.map do |element|
         element.attributes["id"].value.split("-")[1]
