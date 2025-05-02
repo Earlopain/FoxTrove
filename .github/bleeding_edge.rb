760527eef054ce9f887886bc665b53b8ab4fc4d1
Earlopain <14981592+Earlopain@users.noreply.github.com>
2025-05-02 11:53:20 +0200

    :thinking:

--- .github/bleeding_edge.rb
+++ .github/bleeding_edge.rb
@@ -4,12 +4,14 @@ require "json"
 remote = "https://rubygems.org"
 fetcher = Bundler::Fetcher.new(Bundler::Source::Rubygems::Remote.new(remote))
 dsl = Bundler::Dsl.evaluate("#{__dir__}/../Gemfile", "#{__dir__}/../Gemfile.lock", {})
-stubs = dsl.resolve.materialize(dsl.dependencies_for(%w[default test])).map do |stub|
+dsl.resolve_remotely!
+stubs = dsl.specs_for(%w[default test]).map do |stub|
   Bundler::RemoteSpecification.new(stub.name, stub.version, stub.platform, fetcher)
 end
 
 lines = ["source #{remote.inspect}"]
 NO_GIT = [
+  "bundler", # The running version of Bundler (2.7.0.dev) does not match...
   "minitest", # https://github.com/minitest/minitest/issues/750 (lol)
 ].freeze
 FULL_SKIP = [
