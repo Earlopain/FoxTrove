d43fe88b2fa5a6bdec14a19893f2ea17e31a3f10
Earlopain <14981592+Earlopain@users.noreply.github.com>
2025-05-20 10:03:31 +0200

    Ignore one more warning for now
    I guess this manifests because it's loaded before rails. But I haven't looked much into that yet.

--- config/boot.rb
+++ config/boot.rb
@@ -3,6 +3,7 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 $VERBOSE = 1
 def Warning.warn(msg, ...)
   return if msg.include?("rouge") # https://github.com/rouge-ruby/rouge/issues/1961
+  return if msg.match?(/circular require.*rails-html-sanitizer/) # Unknown, probably load order
 
   raise StandardError, msg
 end
