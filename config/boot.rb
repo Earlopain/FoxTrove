a21a1ece71519d3c9a15a3619c3647569764a52a
Earlopain <14981592+Earlopain@users.noreply.github.com>
2025-08-23 13:01:27 +0200

    Also print the error message instead of just raising
    The exception may be swallowed, making it incredibly hard to figure out what happened

--- config/boot.rb
+++ config/boot.rb
@@ -5,6 +5,7 @@ def Warning.warn(msg, ...)
   return if msg.include?("rouge") # https://github.com/rouge-ruby/rouge/issues/1961
   return if msg.match?(/circular require.*rails-html-sanitizer/) # Unknown, probably load order
 
+  super
   raise StandardError, msg
 end
 
