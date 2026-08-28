4dcd8d1231daf0ac900d828864d115e0f66e972d
Earlopain <14981592+Earlopain@users.noreply.github.com>
2026-08-28 15:33:58 +0200

    Ignore warnings from `Coverage.line_stub`
    I was running a different ruby locally and didn't notice the issue.
    See https://bugs.ruby-lang.org/issues/22269 for details

--- config/boot.rb
+++ config/boot.rb
@@ -4,6 +4,7 @@ $VERBOSE = 1
 def Warning.warn(msg, ...)
   return if msg.include?("rouge") # https://github.com/rouge-ruby/rouge/issues/1961
   return if msg.match?(/circular require.*rails-html-sanitizer/) # Unknown, probably load order
+  return if caller.any? { it.include?("Coverage.line_stub") } # https://bugs.ruby-lang.org/issues/22269
 
   super
   raise StandardError, msg
